4781c007f642a74d99dcf1cc3c0c438718ddb993
lucianoezequieldacosta <lu_taladro@hotmail.com>
2017-02-01 11:58:24 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,13 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var nombre;//define variable
+	nombre="luciano";//cargo variable
+    nombre= prompt("ingrese su nombre","natalia natalia");
+
+	alert("nombre: "  +nombre);
+//esto es un comentario de una niña
+	var person = prompt("ingresa tu nombre", "natalia natalia");
+
 }
 
