a8ad1ca3c28078b1d5128497d6502ad32265bd56
lucianoezequieldacosta <lu_taladro@hotmail.com>
2017-02-08 12:13:47 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,27 @@
 function Mostrar()
 {
 //tomo la edad  
-
+ var edad;
+ var resultado;
+ edad=parseInt(document.getElementById('edad').value);
+ if (edad <18)
+    {
+    	if (edad >12)
+    	{
+    		alert("Es un adolescente");
+    	}
+    }
+   
+    if (edad >17)
+    {
+    	alert("Eres un adulto");
+    }
+    if (edad <13)
+    {
+    	alert("Eres un niño");
+    }
+// if (edad >12 && edad <18) En vez del primer IF 
+// Igual funciona
 
 
 
