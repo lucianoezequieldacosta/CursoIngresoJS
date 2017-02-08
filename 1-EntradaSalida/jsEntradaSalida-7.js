9f6ba094e1b366f5ff848d7e713aa47f62f63e8e
lucianoezequieldacosta <lu_taladro@hotmail.com>
2017-02-07 21:47:54 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,59 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var resultado;
+	var numero1;	
+	var numero2;
+	numero1 = document.getElementById('numeroUno').value;
+    numero2 = document.getElementById('numeroDos').value;
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+    resultado = numero1 + numero2;
+    alert("El resultado es: " + resultado);
+
+
+
+
 }
 
 function restar()
 {
-	
+	var resultado;
+	var numero1;
+	var numero2;
+	numero1 = document.getElementById('numeroUno').value;
+    numero2 = document.getElementById('numeroDos').value;
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+    resultado = numero1 - numero2;
+    alert("El resultado es: " + resultado);
+
 }
 
 function multiplicar()
 { 
+	var multiplicar;
+	var numero1;
+	var numero2;
+	numero1 = document.getElementById('numeroUno').value;
+    numero2 = document.getElementById('numeroDos').value;
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+    resultado = numero1 * numero2;
+    alert("El resultado es: " + resultado);
+
 	
 }
 
 function dividir()
 {
-	
+	var numero1;
+	var numero2;
+	numero1 = document.getElementById('numeroUno').value;
+    numero2 = document.getElementById('numeroDos').value;
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+    resultado = numero1 / numero2;
+    alert("El resultado es: " + resultado);
 }
 
