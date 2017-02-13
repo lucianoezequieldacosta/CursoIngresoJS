9185d6353043bddc776ea466a027260bd2b812df
lucianoezequieldacosta <lu_taladro@hotmail.com>
2017-02-13 16:40:31 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,5 +1,15 @@
 function Mostrar()
 {
+   var edad;
+   var resultado;
+   var soltero;
+   edad=parseInt(document.getElementById('edad').value);
+   soltero=document.getElementById('estadoCivil').value;
+   if (edad <18 && soltero !='soltero')
+   { 
+    alert("eres muy pequeño para no ser soltero");
+   } 
+
 //tomo la edad  
 
 	
