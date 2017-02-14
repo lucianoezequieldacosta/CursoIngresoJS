128a1a334932f1ad0369c4f55dc495e3f6c043c3
lucianoezequieldacosta <lu_taladro@hotmail.com>
2017-02-14 11:35:44 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,8 +3,27 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch (mesDelAño) {
 
+    case "Enero":
+    case "Febrero":    
+    case "Marzo":
+    case "Abril":
+    case "Mayo" :
+    case "Junio":
+        alert("falta para el invierno");
+        break;
+    case "Julio":
+    case "Agosto":
+        alert("Abrigate que hace frio");
+        break;
+    case "Septiembre":
+    case "Octubre":
+    case "Noviembre":
+    case "Diciembre":
+        alert("Ya pasamos el frio, ahora calor!!!");
+        break;
+    }  
 
 
 
