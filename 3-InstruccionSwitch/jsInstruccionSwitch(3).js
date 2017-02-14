0bcd832bebd273674c6050e1917391df3e68efe3
lucianoezequieldacosta <lu_taladro@hotmail.com>
2017-02-14 12:06:19 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -2,6 +2,31 @@ function Mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+switch (mesDelAño) {
+
+	case "Febrero":
+        alert("Este mes no tiene mas de 29 días")
+	    break;
+    case "Enero":
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+    case "Junio":
+    case "Julio":
+    case "Agosto":
+    case "Septiembre":
+    case "Octubre":
+    case "Noviembre":
+    case "Diciembre":
+        alert("Este mes tiene 30 o más días");
+        break;
+}
+
+
+
+
+
+
 
 //alert (mesDelAño);
 	
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -2,7 +2,24 @@ function Mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+switch (mesDelAño) {
+	case "Enero"
+        alert("Este mes tiene 31 dias")
+	    break;
+    case "Marzo": 
 
+    case "Abril":
+    case "Mayo":
+    case "Junio":
+    case "Julio":
+    case "Agosto":
+    case "Septiembre":
+    case "Octubre":
+    case "Noviembre":
+    case "Diciembre":
+        alert("Este mes tiene 30 o más días");
+        break;
+}
 //alert (mesDelAño);
 	
 	
