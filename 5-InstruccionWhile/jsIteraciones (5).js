2c354a131b373cb4bcf2e7d79ed257cad0657368
lucianoezequieldacosta <lu_taladro@hotmail.com>
2017-02-15 12:12:07 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -2,9 +2,13 @@ function Mostrar()
 {
 
 var sexo = prompt("ingrese f ó m .");
-
-
+    while (sexo !="f" && sexo!="m")
+{
+	sexo = prompt("ingrese un dato valido")
+}
 
 document.getElementById('Sexo').value=sexo;
+
+alert("Gracias por ingresar un dato valido")
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,30 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var promedio;
+	var numero;
+	while (contador <5)
+	{
+        contador=contador+1
+        numero=prompt("ingrese numero")
+        numero=parseInt(numero);
+        acumulador = acumulador + numero;
 
-
-
+	}
+    while(numero <-10 || numero >10)
+   {
+   	numero = prompt ("Error, reingrese el numero")
+   	numero = parseInt(numero);
+   }
+   if (numero <0)
+   {
+   	 negativo = negativo * numero;
+   }
+   else 
+   {
+   	acumulador= acumulador + numero;
+   }
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
-
+//parsear la suma
 }//FIN DE LA FUNCIÓN
