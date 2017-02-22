6532bcc9589c5b4b945c511f89b4540136b8f344
lucianoezequieldacosta <lu_taladro@hotmail.com>
2017-02-22 12:04:31 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,8 @@
 function Mostrar()
 {
-
-
-
+//al generar numeros random de 0 a 10 se debe contar la cantidad de cada uno de estas cifras, repetir la iteracion para lanzar el random cien mil veces.
+// e informar 1) la cantidad de veces que salio cada numero del 0 al 10
+// 2) el porcentaje de veces que salio cada numero con respecto al total 
+// 
 
 }//FIN DE LA FUNCIÓN
