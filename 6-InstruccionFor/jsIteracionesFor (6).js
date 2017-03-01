35eaa69dd58e7f09472b4c7e17601164547c7b76
lucianoezequieldacosta <lu_taladro@hotmail.com>
2017-03-01 11:59:41 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,5 +1,24 @@
 function Mostrar()
 {
+var numeroIngresado;
+var numerosPares;
+var contador;
+var acumulador;
+
+numeroIngresado = prompt ("Ingrese numero")
+numeroIngresado = parseInt(numeroIngresado)
+
+ if (numeroIngresado%2==0)
+ {
+ 	alert("El número es par");
+ }
+else
+  {
+   alert("El número es impar");
+  }
+   
+ 
+
 
 
 
