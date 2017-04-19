c6da4dd4d2544986a1322126f7ee7c45f4bb0797
lucianoezequieldacosta <lu_taladro@hotmail.com>
2017-04-19 11:18:24 -0300

    Add files via upload

---
+++ Ejercicio1/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int legajo;
+    char nombre[20];
+    char apellido[30];
+    int i;
+
+}
+
+int main ()
+{
+   int i;
+   for (i= )
+
+    struct agenda alumno;
+    printf("Ingrese un legajo: ");
+    scanf ("%d", &unAlumno.legajo);
+    printf("Ingrese nombre: ");
+    fflush(stdin);
+    gets(unAlumno.nombre);
+    printf("Ingrese apellido: ");
+    fflush(stdin);
+    scanf("%s", unAlumno.apellido);
+    printf("Ingrese promedio: ");
+    scanf("%f", &unAlumno.promedio);
+    printf("Ingrese dia de nacimiento: ");
+
+
+
+    return (0);
+}
+
+
---
+++ EjercicioEstructura/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int legajo;
+    char nombre[20];
+    char apellido[30];
+
+};
+int main ()
+{
+    int i;
+    for (i=0; i<30; i++)
+
+    struct alumno;
+    printf("Ingrese nombre: ");
+    fflush(stdin);
+    gets(alumno.nombre);
+    printf("Ingrese apellido: ");
+    fflush(stdin);
+    scanf("%s", alumno.apellido);
+    printf("Ingrese promedio: ");
+    scanf("%f", &alumno.promedio);
+    printf("Ingrese un legajo: ");
+    scanf ("%d", &alumno.legajo);
+    strcpy(alumno.nombre, alumno.apellido, alumno.promedio, alumno.legajo);
+
+
+
+    return (0);
+}
+
